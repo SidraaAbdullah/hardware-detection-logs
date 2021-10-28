4e5c735a39fea92aef6486de28cb559ba9e4594a
MuhammadSalmanSiddiqui <salmansidd991@gmail.com>
2021-10-28 12:38:31 +0500

    battery info update

---
+++ battery-percentage.js
@@ -0,0 +1,19 @@
+let batteryPromise = navigator.getBattery();
+batteryPromise.then(batteryCallback);
+function printBatteryStatus(batteryObject) {
+  document.getElementById(
+    "battery-charging-state"
+  ).innerHTML = `Battery charging: ${batteryObject.charging ? "Yes" : "No"}`;
+  document.getElementById("battery-percentage").innerHTML = `Battery level: ${
+    batteryObject.level * 100
+  }%`;
+}
+function batteryCallback(batteryObject) {
+  printBatteryStatus(batteryObject);
+  batteryObject.addEventListener("chargingchange", function (ev) {
+    printBatteryStatus(batteryObject);
+  });
+  batteryObject.addEventListener("levelchange", function (ev) {
+    printBatteryStatus(batteryObject);
+  });
+}
