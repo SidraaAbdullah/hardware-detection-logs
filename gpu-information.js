640e528f0e2658a70c5b5d99ad175a356eb98358
SidraAbdullah <sidraabdullah56@gmail.com>
2021-11-07 19:45:30 +0500

    updates

--- gpu-information.js
+++ gpu-information.js
@@ -1,8 +1,12 @@
 const electron = require("electron");
 // Importing the app module using Electron remote
 const app = electron.remote.app;
-
+var xValues = ["Browser usage", "GPU usage", "Tab usage"];
+let yValues = [];
+var barColors = ["red", "green", "blue"];
 setInterval(function () {
+  yValues = [];
+  document.getElementById("myChart").innerHTML = "";
   document.getElementById("gpu-info").innerHTML = "";
   app
     .getAppMetrics()
@@ -13,17 +17,38 @@ setInterval(function () {
           " , " +
           val.type +
           " usage: " +
-          val.cpu.percentCPUUsage
+          val.cpu.percentCPUUsage,
+        val.cpu.percentCPUUsage
       )
     );
 }, 5000);
 
-function checkGPU(message) {
+function checkGPU(message, value) {
   var el = document.getElementById("gpu-info");
   var newItem = document.createElement("LI");
   var textnode = document.createTextNode(message);
   newItem.appendChild(textnode);
   el.appendChild(newItem);
+  yValues.push(value);
+  new Chart("myChart", {
+    type: "bar",
+    data: {
+      labels: xValues,
+      datasets: [
+        {
+          backgroundColor: barColors,
+          data: yValues,
+        },
+      ],
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: "",
+      },
+    },
+  });
 }
 
 function gpuAppMetric() {
