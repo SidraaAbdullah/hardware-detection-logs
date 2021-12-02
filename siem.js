cfee9e99c79504529510fabd8b54c2ad8d0cd8c0
MuhammadSalmanSiddiqui <salmansidd991@gmail.com>
2021-12-02 10:27:26 +0500

    updtes

--- siem.js
+++ siem.js
@@ -1,11 +1,41 @@
 var hound = require("hound");
+var usb = require("usb");
+const nodeDiskInfo = require("node-disk-info");
+
 var watcher;
-function StartWatcher(path) {
-  // document.getElementById("start").disabled = true;
+let path;
+
+usb.on("attach", function (device) {
+  document.getElementById("errorLogger").innerHTML = "";
+  document.getElementById("messageLogger").innerHTML = `Directory loading...`;
+  setTimeout(() => {
+    nodeDiskInfo
+      .getDiskInfo()
+      .then((disks) => {
+        path = disks.slice(-1)[0]._mounted;
+        StartWatcher(path, device);
+      })
+      .catch((reason) => {
+        console.error(reason);
+      });
+  }, 5000);
+});
+
+usb.on("detach", function (device) {
+  if (path) {
+    watcher.unwatch(path);
+  }
+  document.getElementById("stop").style.display = "none";
+  document.getElementById("errorLogger").innerHTML = "Nothing is being watched";
+  document.getElementById("messageLogger").innerHTML = "";
+});
+
+function StartWatcher(path, device) {
   document.getElementById("stop").style.display = "block";
+  document.getElementById("usb-logs").innerHTML = "";
   document.getElementById(
     "messageLogger"
-  ).innerHTML += `<br> Current Watching Path: ${path}`;
+  ).innerHTML = `<br> Current Watching Path: ${path}`;
   document.getElementById("errorLogger").innerHTML = "";
   watcher = hound.watch(path);
   watcher.on("create", function (file, stats) {
@@ -19,36 +49,9 @@ function StartWatcher(path) {
   });
 }
 
-// // Unwatch specific files or directories.
-// watcher.unwatch("/tmp/another_file");
-// // Unwatch all watched files and directories.
-// watcher.clear();
-
-document.getElementById("start").addEventListener(
-  "click",
-  function (e) {
-    const { dialog } = require("electron").remote;
-    dialog.showOpenDialog(
-      {
-        properties: ["openDirectory"],
-      },
-      function (path) {
-        if (path) {
-          StartWatcher(path[0]);
-        } else {
-          console.log("No path selected");
-        }
-      }
-    );
-  },
-  false
-);
-
 document.getElementById("stop").addEventListener(
   "click",
   function (e) {
-    watcher.clear();
-    document.getElementById("start").disabled = false;
     document.getElementById("stop").style.display = "none";
     document.getElementById("errorLogger").innerHTML =
       "Nothing is being watched";
